73debbb0fb1f7533cc25e64f1c1d70b232095ad9
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-02-27 21:05:10 -0600

    update macro

--- macros/get_feature_event_columns.sql
+++ macros/get_feature_event_columns.sql
@@ -11,8 +11,7 @@
     {"name": "server_name", "datatype": dbt.type_string()},
     {"name": "timestamp", "datatype": dbt.type_timestamp()},
     {"name": "user_agent", "datatype": dbt.type_string()},
-    {"name": "visitor_id", "datatype": dbt.type_string()},
-    {"name": "_fivetran_id", "datatype": dbt.type_string()}
+    {"name": "visitor_id", "datatype": dbt.type_string()}
 ] %}
 
 {{ fivetran_utils.add_pass_through_columns(columns, var('pendo__feature_event_pass_through_columns')) }}
