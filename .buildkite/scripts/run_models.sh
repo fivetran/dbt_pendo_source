533b45dbcd5128de7bb56899110e1a529d630884
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-06-24 14:22:29 -0500

    Update run_models.sh

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -20,5 +20,6 @@ dbt seed --target "$db" --full-refresh
 dbt compile --target "$db"
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
+dbt source freshness --target "$db" || echo "...Only verifying freshness runs..."
 
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
